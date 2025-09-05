4f58cf6a705e83e9c3240f4a6fc708baeb9fd5bd
Skandagarwal <skandagarwal0503@gmail.com>
2025-09-05 16:25:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log("server started at 3000")
+});  
+
+app.get('/', (req,res) =>{
+    res.send("HELLO JIIII");
+});
+
+app.post('/cars', (req,res) =>{
+    const{name,brand} = req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("car submitted succesfully")
+});
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDatabase', {
+    useNewurlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{console.log("connection succcessful")})
+.catch(()=>{console.log("recieved an error")});
